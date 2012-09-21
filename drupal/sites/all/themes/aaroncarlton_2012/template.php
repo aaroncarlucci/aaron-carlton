3beeb0f0abbfda0213ca23d56e75bf6846bcbac3
Aaron Carlton <aaron.r.carlton@gmail.com>
2012-09-21 02:07:37 -0600

    tweaking twitter block output

--- drupal/sites/all/themes/aaroncarlton_2012/template.php
+++ drupal/sites/all/themes/aaroncarlton_2012/template.php
@@ -1,2 +1,8 @@
 <?php
 
+/**
+ * Preprocess twitter_block_tweet.
+ */
+function aaroncarlton_2012_preprocess_twitter_block_tweet(&$vars) {
+  $vars['date'] = date("M d, Y", strtotime($vars['tweet']->created_at)); 
+} 
